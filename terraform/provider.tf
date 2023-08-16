618829426d46a4408ca77fcef3b1b5a15b77e2e1
Chris Moran <au-chrismor@users.noreply.github.com>
2023-08-16 16:59:22 +1000

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,10 +1,17 @@
 terraform {
   required_providers {
     github = {
-        source = "integrations/github"
-        version = "~> 5.0"
+      source = "integrations/github"
+      version = "~> 5.0"
     }
   }
+  cloud {
+    organization = "chrismoran64"
+      workspaces {
+        name = "emergent-devops"\
+      }
+  }
+
 }
 
 provider "github" {
